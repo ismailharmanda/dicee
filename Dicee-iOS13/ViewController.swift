68bf21156cba1ad9467ae91ea2854cc846da4c3f
ismail <iharmanda@hotmail.com>
2022-12-22 01:36:33 +0300

    change dice name with refactor method which refactor all connected nodes

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
